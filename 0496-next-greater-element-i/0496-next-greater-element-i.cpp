dc5db6321b5b7f3f5e66025fc6843ec70aa53805
HarshGalav <111487157+HarshGalav@users.noreply.github.com>
2025-02-07 12:05:07 +0530

    Time: 0 ms (100%), Space: 12.3 MB (81.19%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>nge(nums2.size(),-1);
+        vector<int>v(nums2.size(),-1);
         stack<int>st;
-        for(int i=nums2.size()-1;i>=0;i--){
-            while(!st.empty()&& st.top()<=nums2[i]){
-                st.pop();
-            }
-            if(!st.empty()){
-                nge[i]=st.top();
-            }
+        int n2=nums2.size();
+        for(int i=n2-1;i>=0;i--){
             
+                while(!st.empty() &&st.top()<=nums2[i] ){
+                    st.pop();
+                }
+            
+            if(!st.empty()){
+                v[i]=st.top();
+            }
             st.push(nums2[i]);
         }
         vector<int>ans(nums1.size());
-        for(int i=0;i<nums1.size(); i++){
+        for(int i=0;i<nums1.size();i++){
             for(int j=0;j<nums2.size();j++){
                 if(nums1[i]==nums2[j]){
-                    ans[i]=nge[j];
+                    ans[i]=(v[j]);
                     break;
                 }
             }
