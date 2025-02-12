2c869a3963f7e68447b35b57ef4b1e705e035ac7
HarshGalav <111487157+HarshGalav@users.noreply.github.com>
2025-02-13 00:03:08 +0530

    Time: 35 ms (42.73%), Space: 64 MB (61.17%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
+int f(int n){
+    int ans=0;
+    while(n>0){
+        ans=ans+n%10;
+        n=n/10;
+    }
+    return ans;
+}
     int maximumSum(vector<int>& nums) {
-        unordered_map<int,vector<int>>mp;
-        for(auto i:nums){
-            int temp=i;
-            int sum=0;
-            while(i!=0){
-                sum=sum+i%10;
-                i=i/10;
-            }  
-            mp[sum].push_back(temp);
+        int n=nums.size();
+        int result=-1;
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+           int ds=f(nums[i]);
+           if(mp.count(ds)){
+            result=max(result,nums[i]+mp[ds]);
+           }
+           mp[ds]=max(mp[ds],nums[i]);
         }
-        int sum=0;
-        int flag=0;
-        for(auto i:mp){
-            
-            
-            if(i.second.size()>1){
-                sort(i.second.begin(),i.second.end());
-            reverse(i.second.begin(),i.second.end());
-                flag=1;
-           
-            sum=max(sum,i.second[0]+i.second[1]);
-            }
-            
-        }
-        if(flag==1){
-            return sum;
-        }
-        return -1;
-
+        return result;
     }
 };
