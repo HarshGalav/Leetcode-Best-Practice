1e593b655318cf84f3e8b7ccabc4843c86d58093
HarshGalav <111487157+HarshGalav@users.noreply.github.com>
2025-02-18 22:57:44 +0530

    Time: 23 ms (12.78%), Space: 7.8 MB (73.43%) - LeetHub

---
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool match(string &num,string &pattern){
+        for(int i=0;i<pattern.length();i++){
+            if((pattern[i]=='I' && num[i]>num[i+1])|| (pattern[i]=='D' && num[i]<num[i+1])){
+                return false;
+            }
+        }
+        return true;
+    }
+    string smallestNumber(string pattern) {
+        int n=pattern.length();
+        string num="";
+        for(int i=1;i<=n+1;i++){
+            num.push_back(i+'0');
+        }
+        while(!match(num,pattern)){
+            next_permutation(num.begin(),num.end());
+        }
+         return num;
+    }
+   
+};
