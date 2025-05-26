05d825edab83de86b4b2391507a0ef5eaa05f274
HarshGalav <111487157+HarshGalav@users.noreply.github.com>
2025-05-26 13:42:55 +0530

    Time: 0 ms (100%), Space: 44.8 MB (74.18%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -3,15 +3,13 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
+        int n=g.size(),m=s.size();
         int i=0,j=0;
-        while(j<s.size() && i< g.size()) {
+        while(j<m && i<n){
             if(g[i]<=s[j]){
                 i++;
-                
             }
-            
-                j++;
-       
+            j++;
         }
         return i;
     }
