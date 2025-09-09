b801a4044cd2ee60e3ab7b6d654ee4e91b642ee5
HarshGalav <111487157+HarshGalav@users.noreply.github.com>
2025-09-09 18:53:42 +0530

    Time: 10 ms (7.22%), Space: 12.4 MB (81.08%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>v(nums2.size(),-1);
+        int n=nums2.size();
+        vector<int>res(n,-1);
         stack<int>st;
-        int n2=nums2.size();
-        for(int i=n2-1;i>=0;i--){
-            
-                while(!st.empty() &&st.top()<=nums2[i] ){
-                    st.pop();
-                }
-            
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums2[i]>=st.top()){
+                st.pop();
+            }
             if(!st.empty()){
-                v[i]=st.top();
+                res[i]=st.top();
             }
             st.push(nums2[i]);
         }
@@ -19,11 +17,11 @@ public:
         for(int i=0;i<nums1.size();i++){
             for(int j=0;j<nums2.size();j++){
                 if(nums1[i]==nums2[j]){
-                    ans[i]=(v[j]);
-                    break;
+                    ans[i]=res[j];
                 }
             }
         }
         return ans;
+
     }
 };
