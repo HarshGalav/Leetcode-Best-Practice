69986ed57ae365d89ccf4c71c790537daa7df6f9
HarshGalav <111487157+HarshGalav@users.noreply.github.com>
2025-09-10 12:22:56 +0530

    Time: 7 ms (99.21%), Space: 139.2 MB (59.73%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -4,21 +4,16 @@ public:
         deque<int>dq;
         vector<int>ans;
         for(int i=0;i<nums.size();i++){
-            if((!dq.empty()) && (dq.front()==i-k)){
-                
-                dq.pop_front();
-                
-            }
-            while((!dq.empty() )&& (nums[i] > nums[dq.back()]) ){
+            while(!dq.empty() && nums[dq.back()]<=nums[i]){
                 dq.pop_back();
-                
             }
             dq.push_back(i);
-            if(i >= k-1){
+            if(dq.front()<= i-k){
+                dq.pop_front();
+            }
+            if(i >=k-1){
                 ans.push_back(nums[dq.front()]);
             }
-
-            
         }
         return ans;
     }
