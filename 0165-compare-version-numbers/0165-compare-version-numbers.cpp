8ece57d56f5165055d3abf0eb4469e5f75bc2e51
HarshGalav <111487157+HarshGalav@users.noreply.github.com>
2025-09-23 11:52:29 +0530

    Time: 2 ms (8.95%), Space: 8.7 MB (16.14%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    vector<string>gett(string version){
-        stringstream ss(version);
-        string token="";
-        vector<string>tokens;
-        while(getline(ss,token,'.')){
-            tokens.push_back(token);
+    vector<int>f(string s){
+        int i=0,j=0;
+        vector<int>temp;
+        while(j<s.size()){
+            if(s[j]=='.'){
+                string sub=s.substr(i,j-i);
+                temp.push_back(stoi(sub));
+                i=j+1;
+            }
+            j++;
         }
-        return tokens;
+
+         if (i < s.size()) {
+            string sub = s.substr(i);
+            temp.push_back(stoi(sub));
+        }
+        return temp;
     }
     int compareVersion(string version1, string version2) {
-        vector<string>v1=gett(version1);
-        vector<string>v2=gett(version2);
-        
-        int m=v1.size();
-        int n=v2.size();
-        int i=0;
-        while(i<m || i<n){
-            int a= (i<m )? stoi(v1[i]) : 0;
-            int b= (i<n) ? stoi(v2[i]) : 0;
-            if(a<b){
-                return -1;
-            }
-            if(a>b){
-                return 1;
-            }
-            
-                i++;
-            
+        vector<int>sub1=f(version1);
+        vector<int>sub2=f(version2);
+        int n = max(sub1.size(), sub2.size());
+        for (int i = 0; i < n; i++) {
+            int v1 = (i < sub1.size() ? sub1[i] : 0);
+            int v2 = (i < sub2.size() ? sub2[i] : 0);
+            if (v1 > v2) return 1;
+            if (v1 < v2) return -1;
         }
         return 0;
     }
