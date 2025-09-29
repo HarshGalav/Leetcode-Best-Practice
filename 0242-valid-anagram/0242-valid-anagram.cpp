defe9df7d118f83085b9601ad8cc3e62bb03b021
HarshGalav <111487157+HarshGalav@users.noreply.github.com>
2025-09-29 13:16:46 +0530

    Time: 2 ms (56.44%), Space: 10 MB (11.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size()){
-            return false;
+        unordered_map<char,int>mp;
+        for(auto i:s){
+            mp[i]++;
         }
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        for(int i=0;i<s.size();i++){
-            if(s[i]!=t[i]){
+        for(auto i:t){
+            mp[i]--;
+        }
+        for(auto i:mp){
+            if(i.second != 0){
                 return false;
             }
         }
